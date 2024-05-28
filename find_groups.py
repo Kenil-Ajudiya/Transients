c2b45d26727b5d0af21cdf6e7defb0f8e2444064
CsanadHorvath <csanadhorvath2002@gmail.com>
2024-05-28 12:23:21 +0800

    bruh

--- find_groups.py
+++ find_groups.py
@@ -18,7 +18,10 @@ import sys
 # fname_list = [f'/media/septagonic/CORSAIR/gxarchive/{obsid}/{obsid}_islands_selected.fits' for obsid in obslist.obsid]
 
 # fname_list = ['/home/septagonic/Documents/Transients/islands_setonix/' + fname for fname in os.listdir('/home/septagonic/Documents/Transients/islands_setonix/')]
-fname_list = [f'{sys.argv[1]}/{fname}/{fname}_{sys.argv[2]}' for fname in os.listdir(sys.argv[1])]
+if len(sys.argv == 3):
+    fname_list = [f'{sys.argv[1]}/{fname}/{fname}_{sys.argv[2]}' for fname in os.listdir(sys.argv[1])]
+else:
+    fname_list = [sys.argv[1]]
 
 # obslist = pd.read_csv('100_obs.csv')
 # fname_list = [f'/media/septagonic/CORSAIR/gxarchive/{x}/{x}_4_islands.fits' for x in obslist.obsid]
@@ -100,7 +103,7 @@ group_lengths = [len(np.unique([cand['cand']['obs_id'] for cand in group])) for 
 sort_idx = np.argsort(group_lengths)
 groups = [groups[i] for i in sort_idx]
 for group in groups:
-    if True: #len(group) > 1:
+    if len(group) > 0:
         for row in group:
             cand = row['cand']
             coord = SkyCoord(ra=cand['ra_deg'], dec=cand['dec_deg'], unit='deg', frame='fk5')
