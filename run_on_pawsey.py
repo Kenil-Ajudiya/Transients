12d5dba0b742888d4409de318414c166989fd8a1
CsanadHorvath <csanadhorvath2002@gmail.com>
2024-05-30 13:25:49 +0800

    hr

--- run_on_pawsey.py
+++ run_on_pawsey.py
@@ -8,18 +8,24 @@ import gc
 start_idx = int(sys.argv[1])
 end_idx = int(sys.argv[2])
 
+clean_up = False
+do_scp = False
+
 # obsids_fname = 'obsids_shif.txt'
-obsids_fname = 'obsids_groups.txt'
+# obsids_fname = 'obsids_groups.txt'
+obsids_fname = 'Epoch0084_obsids.txt'
 
 with open(obsids_fname, 'r') as file:
     lines = file.readlines()
     obsids = [line.strip() for line in lines]
 
 # main_dir = 'candidates'
-main_dir = 'groups'
+# main_dir = 'groups'
+main_dir = 'Epoch0084_candidates'
 
-true_mask_table = fits.open(os.path.expanduser('~/group_islands.fits'))[1].data
-true_mask = SkyCoord(true_mask_table['ra_deg'], true_mask_table['dec_deg'], unit=(u.deg, u.deg), frame="fk5")
+# true_mask_table = fits.open(os.path.expanduser('~/group_islands.fits'))[1].data
+# true_mask = SkyCoord(true_mask_table['ra_deg'], true_mask_table['dec_deg'], unit=(u.deg, u.deg), frame="fk5")
+true_mask = None
 
 path = os.path.expanduser('~/'+main_dir+'/{0}/{0}_{1}')
 workdir = os.path.expanduser('~/'+main_dir+'/{0}')
@@ -35,16 +41,18 @@ for obsid in obsids[start_idx:end_idx]:
     obs_idx += 1
     try:
         cube_fname = path.format(obsid, 'transient.hdf5')
-        if not os.path.exists(workdir.format(obsid)):
-            os.system('mkdir ~/{0}/{1}'.format(main_dir, obsid))
-            os.system('scp -i id_rsa ubuntu@146.118.68.233:/mnt/gxarchive/Archived_Obsids/{0}/{0}_transient.hdf5 {1}'.format(obsid, cube_fname))
+        if not do_scp or not os.path.exists(workdir.format(obsid)):
+            if do_scp:
+                os.system('mkdir ~/{0}/{1}'.format(main_dir, obsid))
+                os.system('scp -i id_rsa ubuntu@146.118.68.233:/mnt/gxarchive/Archived_Obsids/{0}/{0}_transient.hdf5 {1}'.format(obsid, cube_fname))
             if os.path.isfile(cube_fname):
                 cands = TransientSearch(path, obsid, filters, 'try_1', True, False, max_plots=150, true_mask=true_mask)
-                if len(cands) > 10:
-                    os.system(f'echo {obsid}, {len(cands)} >> bad_obsids.txt')
-                os.system(f'rm {cube_fname}')
-                if os.path.isfile(path.format(obsid, "deep-MFS-image-pb.fits")):
-                    os.system(f'rm {path.format(obsid, "deep-MFS-image-pb.fits")}')
+                # if len(cands) > 10:
+                #     os.system(f'echo {obsid}, {len(cands)} >> bad_obsids.txt')
+                if clean_up:
+                    os.system(f'rm {cube_fname}')
+                    if os.path.isfile(path.format(obsid, "deep-MFS-image-pb.fits")):
+                        os.system(f'rm {path.format(obsid, "deep-MFS-image-pb.fits")}')
             else:
                 os.system(f'rm -r ~/{main_dir}/{obsid}')
     except Exception as e:
