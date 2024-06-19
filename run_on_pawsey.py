68fe772ede1de22446d392b0b04056c518a9f3fa
Csanad Horvath <chorvath@setonix-05.pawsey.org.au>
2024-06-19 09:29:17 +0800

    bruh

--- run_on_pawsey.py
+++ run_on_pawsey.py
@@ -15,7 +15,7 @@ scp_source = 'ubuntu@146.118.68.233:/mnt/gxarchive/Archived_Obsids'
 # obsids_fname = 'obsids_shif.txt'
 # obsids_fname = 'obsids_groups.txt'
 # obsids_fname = 'Epoch0084_obsids.txt'
-obsids_fname = 'test_obsids.txt'
+obsids_fname = 'Epoch0767_obsids.txt'
 
 with open(obsids_fname, 'r') as file:
     lines = file.readlines()
@@ -24,7 +24,7 @@ with open(obsids_fname, 'r') as file:
 # main_dir = 'candidates'
 # main_dir = 'groups'
 # main_dir = 'Epoch0084_candidates'
-main_dir  = 'Epoch0763_candidates'
+main_dir  = 'Epoch0767_candidates'
 
 # true_mask_table = fits.open(os.path.expanduser('~/group_islands.fits'))[1].data
 # true_mask = SkyCoord(true_mask_table['ra_deg'], true_mask_table['dec_deg'], unit=(u.deg, u.deg), frame="fk5")
